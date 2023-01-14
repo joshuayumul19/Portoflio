3ff599317dea016b1ec2b3ea4ce4daf213f2099d
joshuayumul19 <joshuayumul19@gmail.com>
2023-01-14 10:58:21 +0800

    Code refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const barIcon = document.getElementById("mobile_menu")
 barIcon.addEventListener("click", function () {
-    let navBar = document.getElementById("wrapper");
-    navBar.style.display === "block" ? navBar.style.display = "none" : navBar.style.display = "block";
-});
+	let navBar = document.getElementById("wrapper");
+	navBar.style.display === "block" ? navBar.style.display = "none" : navBar.style.display = "block";
+}
+);
